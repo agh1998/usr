ffd5ed4a530f2aae40c4d70bb4ce89efd25cd55c
henryang <1123482074@qq.com>
2017-01-22 10:49:31 -0800

    add commit

--- create_db.py
+++ create_db.py
@@ -2,6 +2,6 @@ import sqlite3
 
 with sqlite3.connect("users.db") as connection:
 	c = connection.cursor()
-	c.execute("""CREATE TABLE usersFav(usr TEXT, fav TEXT)""")
-	c.execute("""CREATE TABLE usersHis(usr TEXT, his TEXT)""")
+	c.execute("""CREATE TABLE usersFav(usr TEXT, fav TEXT, UNIQUE(usr,fav))""")
+	c.execute("""CREATE TABLE usersHis(usr TEXT, his TEXT, UNIQUE(usr,fav))""")
 	c.close()
