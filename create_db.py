3a4bfe9e98fb653bbf455a2e5526adf2a689124d
henryang <1123482074@qq.com>
2017-01-22 10:50:43 -0800

    debug

--- create_db.py
+++ create_db.py
@@ -3,5 +3,5 @@ import sqlite3
 with sqlite3.connect("users.db") as connection:
 	c = connection.cursor()
 	c.execute("""CREATE TABLE usersFav(usr TEXT, fav TEXT, UNIQUE(usr,fav))""")
-	c.execute("""CREATE TABLE usersHis(usr TEXT, his TEXT, UNIQUE(usr,fav))""")
+	c.execute("""CREATE TABLE usersHis(usr TEXT, his TEXT, UNIQUE(usr,his))""")
 	c.close()
